d46c338f4a59bbb95ebd6f6ed7facb51f29e4d47
Damian Myerscough <Damian.Myerscough@gmail.com>
2022-01-12 18:26:36 -0800

    Initial BadBank

---
+++ src/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+
+const auth = require('./middleware/auth-middleware')
+
+const dal = require('./dal.js');
+
+// create user account
+app.get('/account/create/:name/:email/:password', auth.checkIfAuthenticated, (req, res) => {
+    // check if account exists
+    dal.find(req.params.email).
+        then((users) => {
+
+            // if user exists, return error message
+            if(users.length > 0){
+                console.log('User already in exists');
+                res.send('User already in exists');    
+            }
+            else{
+                // else create user
+                dal.create(req.params.name,req.params.email,req.params.password).
+                    then((user) => {
+                        console.log(user);
+                        res.send(user);            
+                    });            
+            }
+
+        });
+});
+
+
+// login user 
+app.get('/account/login/:email/:password', auth.checkIfAuthenticated, (req, res) => {
+
+    dal.find(req.params.email).
+        then((user) => {
+
+            // if user exists, check password
+            if(user.length > 0){
+                if (user[0].password === req.params.password){
+                    res.send(user[0]);
+                }
+                else{
+                    res.send('Login failed: wrong password');
+                }
+            }
+            else{
+                res.send('Login failed: user not found');
+            }
+    });
+    
+});
+
+// find user account
+app.get('/account/find/:email', auth.checkIfAuthenticated, (req, res) => {
+    dal.find(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+// find one user by email - alternative to find
+app.get('/account/findOne/:email', auth.checkIfAuthenticated, (req, res) => {
+    dal.findOne(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+// update - deposit/withdraw amount
+app.get('/account/update/:email/:amount', auth.checkIfAuthenticated, (req, res) => {
+    const amount = Number(req.params.amount);
+
+    dal.update(req.params.email, amount).
+        then((response) => {
+            console.log(response);
+            res.send(response);
+    });    
+});
+
+// all accounts
+app.get('/account/all', auth.checkIfAuthenticated, (req, res) => {
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+    });
+});
+
+const port = 3000;
+app.listen(port);
+console.log('Running on port: ' + port);
---
+++ src/middleware/auth-middleware.js
@@ -0,0 +1,39 @@
+let admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SVC_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const getAuthToken = (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    req.authToken = req.headers.authorization.split(" ")[1];
+  } else {
+    req.authToken = null;
+  }
+  next();
+};
+
+const checkIfAuthenticated = (req, res, next) => {
+  getAuthToken(req, res, async () => {
+    try {
+      const { authToken } = req;
+      const userInfo = await admin.auth().verifyIdToken(authToken);
+      req.userId = userInfo.user_id;
+      req.userEmail = userInfo.email;
+      return next();
+    } catch (e) {
+      return res
+        .status(401)
+        .send({ error: "You are not authorized to make this request" });
+    }
+  });
+};
+
+module.exports = {
+  checkIfAuthenticated,
+};
